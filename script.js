8d7913995ea390e0058b7d4f282c53536f3d7db5
faiqashahbaz-28 <faiqashahbaz282@gmail.com>
2025-04-23 18:55:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,142 @@
-// making all the nodes accessible
-let boxs = document.querySelectorAll(".box"); 
-let resetBtn = document.querySelector("#reset");
-let msgBox = document.querySelector(".msg_box");
-let newGame = document.querySelector(".new_btn");
-let msgline = document.querySelector("#msg");
+// Get all game elements
+const boxes = document.querySelectorAll(".box");
+const resetBtn = document.querySelector("#reset");
+const msgBox = document.querySelector(".msg_box");
+const newGameBtn = document.querySelector(".new_btn");
+const message = document.querySelector("#msg");
 
+// Game variables
+let isHumanTurn = true; // Human is O, Computer is X
+let movesCount = 0;
+const winPatterns = [
+    [0,1,2], [3,4,5], [6,7,8], // rows
+    [0,3,6], [1,4,7], [2,5,8], // columns
+    [0,4,8], [2,4,6]           // diagonals
+];
+
+// Hide message at start
 msgBox.classList.add("hide");
 
-let turnO = true ;
-let count = 0 ;
-// winning possibilities
-let winnArry = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-];
- //turns of two players
-boxs.forEach  ((box) => {
-    box.addEventListener("click" , () =>{
-        if (turnO == true){
-            box.innerText = "O" ;
-            box.style.color = "blue" ;
-            turnO = false;
-        }else{
-            box.innerText = "X" ;
-            box.style.color = "red" ;
-            turnO = true;
-        }
-        box.disabled = true ;
-        count++;
-        let isWinner = checkWinner ();
-        if(count === 9 && count !== isWinner){
-        drawGame();
-        } 
+// Handle box clicks
+boxes.forEach(box => {
+    box.addEventListener("click", handleHumanMove);
 });
-});
-  
 
-const drawGame = () =>{
-    msgline.innerText = `The game is Draw`;
-        msgBox.classList.remove("hide");
-        disabledboxs();
-}
-// showing winner of the game
-const addWinner = (winner) =>{
-    msgline.innerText = `Congratulations! Winner is ${winner}`;
-    msgBox.classList.remove("hide");
-    disabledboxs();
-}
-// disanbbeling buttons
-const disabledboxs =()=>{
-    for(let box of boxs){
-        box.disabled = true;
+function handleHumanMove() {
+    if (!isHumanTurn || this.textContent !== "") return;
+    
+    // Human makes move
+    this.textContent = "O";
+    this.style.color = "blue";
+    this.disabled = true;
+    movesCount++;
+    
+    // Check game status
+    if (checkWin("O")) {
+        showWinner("O");
+    } else if (movesCount === 9) {
+        showDraw();
+    } else {
+        isHumanTurn = false;
+        setTimeout(computerMove, 500);
     }
 }
-//enabling buttons
-const enabledboxs = ()=>{
-    for(let box of boxs){
-        box.disabled = false;
-        box.innerText ="";
-    }
-}
-//funstion for restarting game
-const playAgain = ()=>{
-    turnO = true ;
-    count = 0 ;
-    enabledboxs();
-    msgBox.classList.add("hide") ;
-}
 
-//on clicking new game button
-newGame.addEventListener("click",()=>{
-    playAgain();
-});
-//on clicking reset button
-resetBtn.addEventListener("click",()=>{
-    playAgain();
-});
-
-//checking the winner of game
-const checkWinner = () =>{
-    for(let pattern of winnArry){
-        let pos1 = boxs[pattern[0]].innerText;
-        let pos2 = boxs[pattern[1]].innerText;
-        let pos3 = boxs[pattern[2]].innerText;
-        
-        if(pos1 != "" && pos2 != "" && pos3 != ""){
-            if(pos1 === pos2 && pos2 === pos3){
-                addWinner(pos1);
+function computerMove() {
+    if (isHumanTurn) return;
+    
+    // Try to win
+    for (let i = 0; i < 9; i++) {
+        if (boxes[i].textContent === "") {
+            boxes[i].textContent = "X";
+            if (checkWin("X")) {
+                finishComputerMove(i);
+                return;
             }
+            boxes[i].textContent = "";
         }
     }
+    
+    // Block human if they can win
+    for (let i = 0; i < 9; i++) {
+        if (boxes[i].textContent === "") {
+            boxes[i].textContent = "O";
+            if (checkWin("O")) {
+                boxes[i].textContent = "X";
+                finishComputerMove(i);
+                return;
+            }
+            boxes[i].textContent = "";
+        }
+    }
+    
+    // Take center if available
+    if (boxes[4].textContent === "") {
+        finishComputerMove(4);
+        return;
+    }
+    
+    // Take a random corner
+    const corners = [0, 2, 6, 8].filter(i => boxes[i].textContent === "");
+    if (corners.length > 0) {
+        finishComputerMove(corners[Math.floor(Math.random() * corners.length)]);
+        return;
+    }
+    
+    // Take any available spot
+    const emptySpots = [...boxes].map((box, i) => box.textContent === "" ? i : null)
+                               .filter(i => i !== null);
+    if (emptySpots.length > 0) {
+        finishComputerMove(emptySpots[Math.floor(Math.random() * emptySpots.length)]);
+    }
 }
+
+function finishComputerMove(index) {
+    boxes[index].textContent = "X";
+    boxes[index].style.color = "red";
+    boxes[index].disabled = true;
+    movesCount++;
+    isHumanTurn = true;
+    
+    if (checkWin("X")) {
+        showWinner("X");
+    } else if (movesCount === 9) {
+        showDraw();
+    }
+}
+
+function checkWin(player) {
+    return winPatterns.some(pattern => 
+        pattern.every(index => boxes[index].textContent === player)
+    );
+}
+
+function showWinner(player) {
+    message.textContent = `Congratulations! ${player} wins!`;
+    msgBox.classList.remove("hide");
+    disableAllBoxes();
+}
+
+function showDraw() {
+    message.textContent = "Game ended in a draw!";
+    msgBox.classList.remove("hide");
+    disableAllBoxes();
+}
+
+function disableAllBoxes() {
+    boxes.forEach(box => box.disabled = true);
+}
+
+function resetGame() {
+    boxes.forEach(box => {
+        box.textContent = "";
+        box.disabled = false;
+    });
+    isHumanTurn = true;
+    movesCount = 0;
+    msgBox.classList.add("hide");
+}
+
+// Button event listeners
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
